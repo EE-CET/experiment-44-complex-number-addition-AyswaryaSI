883872f5a59e38f6f084be96b408e09800fb9fd6
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-06 11:28:15 +0530

    Update ComplexAddition.java

--- src/ComplexAddition.java
+++ src/ComplexAddition.java
@@ -2,22 +2,19 @@ import java.util.Scanner;
 class Complex {
     double real;
     double imaginary;
-
     Complex(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-
     Complex add(Complex c) {
-        double newReal = this.real + c.real;
-        double newImaginary = this.imaginary + c.imaginary;
-        return new Complex(newReal, newImaginary);
+        return new Complex(this.real + c.real, this.imaginary + c.imaginary);
     }
 }
 
 public class ComplexAddition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         double r1 = sc.nextDouble();
         double i1 = sc.nextDouble();
 
@@ -29,7 +26,7 @@ public class ComplexAddition {
 
         Complex result = c1.add(c2);
 
-        System.out.println(result.real + " + " + result.imaginary + "i");
+        System.out.println((int) result.real + " + " + (int) result.imaginary + "i");
 
         sc.close();
     }
